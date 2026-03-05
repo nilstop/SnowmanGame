5645defd7cfdfbb721638f585ef04cc95f662f84
nilstop <nilstopgaard@gmail.com>
2026-03-05 18:21:09 +0100

    Water can slide on floor and walls, not corner facing out

--- snowball.gd
+++ snowball.gd
@@ -1,23 +1,27 @@
 extends Area2D
 
+
 var velocity: Vector2
 var direction: Vector2
 
 
 const GRAVITY = 0.7
 const SPEED = 24.0
-const START_UP_VELOCITY = -5.0
+const START_UP_VELOCITY = -8.0
 
 func _ready() -> void:
+	# Set start velocity, extra velocity upwards if only facing right or left
 	velocity = direction * SPEED
-	if velocity.normalized() == Vector2.LEFT or velocity.normalized() == Vector2.RIGHT:
+	if velocity.normalized().y >= 0:
 		velocity += Vector2(0,START_UP_VELOCITY)
 
 func _physics_process(delta: float) -> void:
 	# Set velocity and move
 	velocity.y += GRAVITY
+	if velocity.y >= 0:
+		velocity.y += GRAVITY * 0.8
 	global_position += velocity
 
 func _on_body_entered(body: Node2D) -> void:
-	if body.is_in_group("floor"):
+	if body.is_in_group("environment"):
 		queue_free()
--- snowman.gd
+++ snowman.gd
@@ -1,45 +1,95 @@
 extends CharacterBody2D
 
+@onready var sprite: Sprite2D = $Sprite2D
+@onready var snowman_shape: CollisionShape2D = $SnowmanShape
+@onready var water_shape: CollisionShape2D = $WaterShape
+
+
+enum States {Snow, Water, Ice, Steam}
+
 @export var snowball: PackedScene
 
+var state: States = States.Snow: set = set_state
 var facing := Vector2.RIGHT
 var throw_direction: Vector2
 
+const PLACEHOLDER_SNOWMAN_SIZE = Vector2(0.4, 0.578)
+const PLACEHOLDER_WATER_SIZE = Vector2(0.2, 0.2)
 const SPEED = 400.0
+const WATER_SPEED = 600.0
 const JUMP_VELOCITY = -650.0
 const ACCELERATION = 0.1
 const DECELERATION = 0.2
 const GRAVITY = 1200
 const RELEASE_VELOCITY = -350.0
 
+func set_state(new_state):
+	water_shape.disabled = true
+	snowman_shape.disabled = true
+	
+	if new_state == States.Water:
+		
+		move_and_collide(Vector2.DOWN * 25)
+		water_shape.disabled = false
+		velocity = Vector2.ZERO
+		sprite.scale = PLACEHOLDER_WATER_SIZE
+		
+	if new_state == States.Snow:
+		
+		snowman_shape.disabled = false
+		
+		sprite.scale = PLACEHOLDER_SNOWMAN_SIZE
+		
+	state = new_state
+
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += GRAVITY * delta
-		if velocity.y >= -200:
-			velocity.y += GRAVITY * 1.1 * delta
+	#region Snowman
+	if state == States.Snow:
+		# Add the gravity.
+		if not is_on_floor():
+			velocity.y += GRAVITY * delta
+			if velocity.y >= -200:
+				velocity.y += GRAVITY * 1.1 * delta
+		# Handle jump.
+		if Input.is_action_just_pressed("jump") and is_on_floor():
+			velocity.y = JUMP_VELOCITY
+		if Input.is_action_just_released("jump") and not is_on_floor() and velocity.y <= RELEASE_VELOCITY:
+			velocity.y = RELEASE_VELOCITY
+		# Get the input direction and handle the movement/deceleration.
+		var direction := Input.get_axis("left", "right")
+		set_facing(direction)
+		if direction:
+			velocity.x = lerp(velocity.x, direction * SPEED, ACCELERATION)
+		else:
+			velocity.x = lerp(velocity.x, 0.0, DECELERATION)
+	#endregion
+	#region water
+	if state == States.Water:
 
-	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-	if Input.is_action_just_released("jump") and not is_on_floor() and velocity.y <= RELEASE_VELOCITY:
-		velocity.y = RELEASE_VELOCITY
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("left", "right")
-	set_facing(direction)
-	if direction:
-		velocity.x = lerp(velocity.x, direction * SPEED, ACCELERATION)
-	else:
-		velocity.x = lerp(velocity.x, 0.0, DECELERATION)
-
+		var x_dir = Input.get_axis("left", "right")
+		var y_dir = Input.get_axis("up", "down")
+		if is_on_floor() or is_on_ceiling():
+			velocity.x = x_dir * WATER_SPEED
+		else:
+			velocity.x = 0
+		if is_on_wall():
+			velocity.y = y_dir * WATER_SPEED
+		else:
+			velocity.y = 0
+	#endregion
 	move_and_slide()
 
 func _process(_delta: float) -> void:
-	if Input.is_action_just_pressed("shoot"):
-		inst(snowball)
+	if state == States.Snow:
+		if Input.is_action_just_pressed("shoot"):
+			inst_snowball(snowball)
+		if Input.is_action_just_pressed("water") and is_on_floor():
+			set_state(States.Water)
+	if state == States.Water:
+		if Input.is_action_just_released("water"):
+			set_state(States.Snow)
 
-func inst(scene: PackedScene):
+func inst_snowball(scene: PackedScene):
 	var instance = scene.instantiate()
 	instance.global_position = global_position
 	instance.direction = get_throw_direction()
